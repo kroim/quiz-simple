bb44d5982c4e21d409415c7abdaf33bdbc1a28f8
kroim <wonderfulplayer@outlook.com>
2021-02-02 22:05:02 +0800

    Update quizzes_all.php

--- views/main/quizzes_all.php
+++ views/main/quizzes_all.php
@@ -13,13 +13,16 @@ include(PREPEND_PATH . "views/partials/header.php");
     #modal_remove_quiz .modal-body {
         text-align: center;
     }
+    .toggle-switch__helper:before {
+        background-color: #949490;
+    }
 </style>
 <body data-sa-theme="10">
 <?php include(PREPEND_PATH . "views/partials/sidebar.php"); ?>
 <section class="content">
 
     <header class="content__title">
-        <h1>All Quizzes</h1>
+        <h1>Quizzes</h1>
         <div class="actions">
             <button class="btn btn-success" onclick="$('#modal_add_quiz').modal()">Create a quiz</button>
         </div>
@@ -32,8 +35,8 @@ include(PREPEND_PATH . "views/partials/header.php");
                     <tr>
                         <th style="width: 10%">No</th>
                         <th style="width: 10%">Code</th>
-                        <th style="width: 30%">Question</th>
-                        <th style="width: 10%">Duration (m)</th>
+                        <th style="width: 30%">Question (duration)</th>
+                        <th style="width: 10%">Total Duration (m)</th>
                         <th style="width: 20%">Actions</th>
                     </tr>
                     </thead>
@@ -42,12 +45,13 @@ include(PREPEND_PATH . "views/partials/header.php");
                         <tr id="quiz_<?php echo $new_quizzes[$i]->id; ?>">
                             <td class="quiz-id"><?php echo $i + 1; ?></td>
                             <td class="quiz-code"><?php echo $new_quizzes[$i]->code; ?></td>
-                            <td class="quiz-question" data-id=`<?php echo json_encode($new_quizzes[$i]->question_ids); ?>`>
+                            <td class="quiz-question" data-id=`<?php echo json_encode($new_quizzes[$i]->question_ids); ?>`
+                                data-duration=`<?php echo json_encode($new_quizzes[$i]->durations); ?>`>
                                 <?php for ($j = 0; $j < count($new_quizzes[$i]->questions); $j++) { ?>
-                                    <div><?php echo $new_quizzes[$i]->questions[$j]; ?></div>
+                                    <div><?php echo $new_quizzes[$i]->questions[$j]; ?> (<?php echo $new_quizzes[$i]->durations[$j]; ?>)</div>
                                 <?php } ?>
                             </td>
-                            <td class="quiz-duration"><?php echo $new_quizzes[$i]->total_duration; ?></td>
+                            <td class="quiz-duration" data-flag="<?php echo $new_quizzes[$i]->total_duration_flag; ?>"><?php echo $new_quizzes[$i]->total_duration; ?></td>
                             <td class="question-action">
                                 <button class="btn btn-warning btn-sm" onclick="editQuiz(<?php echo $new_quizzes[$i]->id; ?>)">Edit</button>
                                 <button class="btn btn-danger btn-sm" onclick="removeQuiz(<?php echo $new_quizzes[$i]->id; ?>)">Remove</button>
@@ -62,30 +66,50 @@ include(PREPEND_PATH . "views/partials/header.php");
 </section>
 <!-- add a question modal -->
 <div class="modal fade" id="modal_add_quiz" tabindex="-1">
-    <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
         <div class="modal-content">
             <div class="modal-header">
                 <h4>Create a quiz</h4>
             </div>
             <div class="modal-body">
                 <form id="modal-add-quiz-form">
-                    <label>Questions</label>
+                    <div class="row">
+                        <div class="col-sm-9"><label>Questions</label></div>
+                        <div class="col-sm-3"><label>Duration (m)</label></div>
+                    </div>
                     <div id="modal_add_questions_div">
                         <div class="form-group question-item">
-                            <select class="form-control page-select" required>
-                                <?php for ($k = 0; $k < count($questions); $k++) { ?>
-                                    <option value="<?php echo $questions[$k]['id'] ?>"><?php echo $questions[$k]['question'] ?></option>
-                                <?php } ?>
-                            </select>
+                            <div class="row">
+                                <div class="col-sm-9">
+                                    <select class="form-control page-select" required>
+                                        <?php for ($k = 0; $k < count($questions); $k++) { ?>
+                                            <option value="<?php echo $questions[$k]['id'] ?>"><?php echo $questions[$k]['question'] ?></option>
+                                        <?php } ?>
+                                    </select>
+                                </div>
+                                <div class="col-sm-3">
+                                    <input type="number" class="form-control">
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="form-group text-right">
                         <button type="button" class="btn btn-link" onclick="addQuestionItem()">Add more question</button>
                         <button type="button" class="btn btn-link" onclick="removeQuestionItem()">Remove one</button>
                     </div>
+                    <label>Total Duration (m)</label>
                     <div class="form-group">
-                        <label>Duration (m)</label>
-                        <input class="form-control" id="modal_add_quiz_duration" type="number" required>
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <div class="toggle-switch toggle-switch--green">
+                                    <input type="checkbox" class="toggle-switch__checkbox" id="modal_add_quiz_total_duration_option">
+                                    <i class="toggle-switch__helper"></i>
+                                </div>
+                            </div>
+                            <div class="col-sm-8">
+                                <input class="form-control" id="modal_add_quiz_total_duration" placeholder="Total duration" type="number" style="display: none;">
+                            </div>
+                        </div>
                     </div>
                     <div class="form-group text-center">
                         <button type="submit" class="btn btn-link">Create a question</button>
@@ -110,23 +134,43 @@ include(PREPEND_PATH . "views/partials/header.php");
                         <label>Code</label>
                         <input type="text" class="form-control" id="modal_edit_quiz_code" readonly>
                     </div>
-                    <label>Questions</label>
+                    <div class="row">
+                        <div class="col-sm-9"><label>Questions</label></div>
+                        <div class="col-sm-3"><label>Duration (m)</label></div>
+                    </div>
                     <div id="modal_edit_questions_div">
                         <div class="form-group question-item">
-                            <select class="form-control page-select" required>
-                                <?php for ($k = 0; $k < count($questions); $k++) { ?>
-                                    <option value="<?php echo $questions[$k]['id'] ?>"><?php echo $questions[$k]['question'] ?></option>
-                                <?php } ?>
-                            </select>
+                            <div class="row">
+                                <div class="col-sm-9">
+                                    <select class="form-control page-select" required>
+                                        <?php for ($k = 0; $k < count($questions); $k++) { ?>
+                                            <option value="<?php echo $questions[$k]['id'] ?>"><?php echo $questions[$k]['question'] ?></option>
+                                        <?php } ?>
+                                    </select>
+                                </div>
+                                <div class="col-sm-3">
+                                    <input type="number" class="form-control">
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="form-group text-right">
                         <button type="button" class="btn btn-link" onclick="addEditQuestionItem()">Add more question</button>
                         <button type="button" class="btn btn-link" onclick="removeEditQuestionItem()">Remove one</button>
                     </div>
+                    <label>Total Duration (m)</label>
                     <div class="form-group">
-                        <label>Duration (m)</label>
-                        <input class="form-control" id="modal_edit_quiz_duration" type="number" required>
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <div class="toggle-switch toggle-switch--green">
+                                    <input type="checkbox" class="toggle-switch__checkbox" id="modal_edit_quiz_total_duration_option">
+                                    <i class="toggle-switch__helper"></i>
+                                </div>
+                            </div>
+                            <div class="col-sm-8">
+                                <input class="form-control" id="modal_edit_quiz_total_duration" placeholder="Total duration" type="number" style="display: none;">
+                            </div>
+                        </div>
                     </div>
                     <div class="form-group text-center">
                         <button type="submit" class="btn btn-link">Save changes</button>
@@ -158,11 +202,18 @@ include(PREPEND_PATH . "views/partials/header.php");
 </div>
 <div id="questions-select-hidden" style="display: none">
     <div class="form-group question-item">
-        <select class="form-control page-select" required>
-            <?php for ($k = 0; $k < count($questions); $k++) { ?>
-                <option value="<?php echo $questions[$k]['id'] ?>"><?php echo $questions[$k]['question'] ?></option>
-            <?php } ?>
-        </select>
+        <div class="row">
+            <div class="col-sm-9">
+                <select class="form-control page-select" required>
+                    <?php for ($k = 0; $k < count($questions); $k++) { ?>
+                        <option value="<?php echo $questions[$k]['id'] ?>"><?php echo $questions[$k]['question'] ?></option>
+                    <?php } ?>
+                </select>
+            </div>
+            <div class="col-sm-3">
+                <input type="number" class="form-control">
+            </div>
+        </div>
     </div>
 </div>
 <input hidden class="_questions" value='<?= json_encode($questions); ?>'/>
@@ -201,18 +252,53 @@ include(PREPEND_PATH . "views/partials/header.php");
         if (questions_length < 2) return;
         $('#modal_edit_questions_div .question-item:nth-child(' + questions_length +')').remove()
     }
+    $('#modal_add_quiz_total_duration_option').on('change', function () {
+        if ($(this).prop('checked')) $('#modal_add_quiz_total_duration').css('display', 'block');
+        else $('#modal_add_quiz_total_duration').css('display', 'none');
+    });
+    $('#modal_edit_quiz_total_duration_option').on('change', function () {
+        if ($(this).prop('checked')) $('#modal_edit_quiz_total_duration').css('display', 'block');
+        else $('#modal_edit_quiz_total_duration').css('display', 'none');
+    });
     $('#modal-add-quiz-form').on('submit', function (e) {
         e.preventDefault();
+        let total_duration_flag = $('#modal_add_quiz_total_duration_option').prop('checked');
         let questions = [];
-        $('#modal_add_questions_div .question-item').each(function (index, item) {
-            let item_id = $(item).find('select').val();
-            questions.push(item_id);
-        });
-        let duration = $('#modal_add_quiz_duration').val();
+        let durations = [];
+        if (total_duration_flag) {
+            $('#modal_add_questions_div .question-item').each(function (index, item) {
+                let item_id = $(item).find('select').val();
+                questions.push(item_id);
+            });
+        } else {
+            let check_question_duration = true;
+            $('#modal_add_questions_div .question-item').each(function (index, item) {
+                let item_id = $(item).find('select').val();
+                let item_dur = $(item).find('input[type="number"]').val();
+                if (!item_dur || item_dur === "0") {
+                    customAlert("Required each question's duration");
+                    check_question_duration = false;
+                    return;
+                }
+                questions.push(item_id);
+                durations.push(item_dur);
+            });
+            if (!check_question_duration) return;
+        }
+        let total_duration = '0';
+        if (total_duration_flag) {
+            total_duration = $('#modal_add_quiz_total_duration').val();
+            if (!total_duration || total_duration === "0") {
+                customAlert("Required total duration");
+                return;
+            }
+        }
         let data = {
             action: "add_quiz_all",
             questions: JSON.stringify(questions),
-            duration: duration,
+            total_duration_flag: total_duration_flag,
+            durations: JSON.stringify(durations),
+            total_duration: total_duration,
         };
         $.ajax({
             url: base_url + "/quizzes-all",
@@ -233,11 +319,23 @@ include(PREPEND_PATH . "views/partials/header.php");
         $('#modal_edit_quiz_id').val(id);
         let code = $('tr#quiz_' + id + ' .quiz-code').text();
         $('#modal_edit_quiz_code').val(code);
-        let duration = $('tr#quiz_' + id + ' .quiz-duration').text();
-        $('#modal_edit_quiz_duration').val(duration);
+        let data_flag = $('tr#quiz_' + id + ' .quiz-duration').attr('data-flag');
+        if (data_flag === "1") {
+            let duration = $('tr#quiz_' + id + ' .quiz-duration').text();
+            $('#modal_edit_quiz_total_duration').val(duration);
+            $('#modal_edit_quiz_total_duration_option').prop('checked', true);
+            $('#modal_edit_quiz_total_duration').css('display', 'block');
+        } else {
+            $('#modal_edit_quiz_total_duration').val(0);
+            $('#modal_edit_quiz_total_duration_option').prop('checked', false);
+            $('#modal_edit_quiz_total_duration').css('display', 'none');
+        }
         let question_ids = $('tr#quiz_' + id + ' .quiz-question').attr('data-id').replace(/`/g, '');
+        let question_durations = $('tr#quiz_' + id + ' .quiz-question').attr('data-duration').replace(/`/g, '');
         question_ids = JSON.parse(question_ids);
+        question_durations = JSON.parse(question_durations);
         $('#modal_edit_questions_div .question-item:nth-child(1) select').val(question_ids[0]);
+        $('#modal_edit_questions_div .question-item:nth-child(1) input').val(question_durations[0]);
         let questions_length = $('#modal_edit_questions_div .question-item').length;
         if (questions_length > 1) {
             for (let k = 1; k < questions_length; k++) {
@@ -245,13 +343,16 @@ include(PREPEND_PATH . "views/partials/header.php");
             }
         }
         for (let i = 1; i < question_ids.length; i++) {
-            let html = '<div class="form-group question-item"><select class="form-control page-select" required>';
+            let html = '<div class="form-group question-item"><div class="row"><div class="col-sm-9"><select class="form-control page-select" required>';
             for (let j = 0; j < _questionArr.length; j++) {
-                if (_questionArr[j].id == question_ids[i])
+                if (_questionArr[j].id == question_ids[i]) {
                     html += '<option value="' + _questionArr[j].id +'" selected>' + _questionArr[j].question + '</option>';
-                else html += '<option value="' + _questionArr[j].id +'">' + _questionArr[j].question + '</option>';
+                } else {
+                    html += '<option value="' + _questionArr[j].id +'">' + _questionArr[j].question + '</option>';
+                }
             }
-            html += '</select></div>';
+            html += '</select></div><div class="col-sm-3"><input type="number" class="form-control" value="' +
+                question_durations[i] +'"></div></div></div>';
             $('#modal_edit_questions_div').append(html);
         }
         $('#modal_edit_quiz').modal();
@@ -259,17 +360,44 @@ include(PREPEND_PATH . "views/partials/header.php");
     $('#modal-edit-quiz-form').on('submit', function (e) {
         e.preventDefault();
         let quiz_id = $('#modal_edit_quiz_id').val();
+        let total_duration_flag = $('#modal_edit_quiz_total_duration_option').prop('checked');
         let questions = [];
-        $('#modal_edit_questions_div .question-item').each(function (index, item) {
-            let item_id = $(item).find('select').val();
-            questions.push(item_id);
-        });
-        let duration = $('#modal_edit_quiz_duration').val();
+        let durations = [];
+        if (total_duration_flag) {
+            $('#modal_edit_questions_div .question-item').each(function (index, item) {
+                let item_id = $(item).find('select').val();
+                questions.push(item_id);
+            });
+        } else {
+            let check_question_duration = true;
+            $('#modal_edit_questions_div .question-item').each(function (index, item) {
+                let item_id = $(item).find('select').val();
+                let item_dur = $(item).find('input[type="number"]').val();
+                if (!item_dur || item_dur === "0") {
+                    customAlert("Required each question's duration");
+                    check_question_duration = false;
+                    return;
+                }
+                questions.push(item_id);
+                durations.push(item_dur);
+            });
+            if (!check_question_duration) return;
+        }
+        let total_duration = '0';
+        if (total_duration_flag) {
+            total_duration = $('#modal_edit_quiz_total_duration').val();
+            if (!total_duration || total_duration === "0") {
+                customAlert("Required total duration");
+                return;
+            }
+        }
         let data = {
             action: "edit_quiz_all",
             quiz_id: quiz_id,
             questions: JSON.stringify(questions),
-            duration: duration,
+            total_duration_flag: total_duration_flag,
+            durations: JSON.stringify(durations),
+            total_duration: total_duration,
         };
         $.ajax({
             url: base_url + "/quizzes-all",
